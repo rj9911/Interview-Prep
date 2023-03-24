6fd77be30fa847ce7571610365ed6835476e27da
Priyam Gupta <priyamgupta880@gmail.com>
2023-03-24 18:33:40 +0530

    Added solution - LeetHub

---
+++ Check whether BST contains Dead End - GFG/check-whether-bst-contains-dead-end.cpp
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+
+
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root) return 0;
+
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+         return 1 + getCountOfNode(root->left, l, h) +
+                    getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+         return getCountOfNode(root->right, l, h);
+
+
+    else return getCountOfNode(root->left, l, h);
+}
+
+
+bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+
+        root = NULL;
+
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+
+        }
+
+        
+     cout<<isDeadEnd(root);
+     cout<<endl;
+    }
+}
+
+
+// } Driver Code Ends
+
+
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+
+/*You are required to complete below method */
+bool solve(Node* root, int min, int max){   
+    if(!root) return false; // No dead end
+    if(min == max) return true; // Dead End => interval gets merged ( 9 , 9 )
+    return solve(root->left, min, root->data-1) or solve(root->right, root->data+1, max); 
+    // (1, 6) + 7 + (8, INT_MAX)
+}
+bool isDeadEnd(Node *root)
+{
+    return solve(root, 1, INT_MAX);   // (1, INT_MAX) interval is possible
+    /* 
+       1,7       9,INT_MAX
+       1,6    9,9     11,INT_MAX  
+    */
+    
+}

1ec24a1424f6f93fb200af53d5090c11924713a9
ninjacoder <priyamgupta880@gmail.com>
2024-06-07 17:23:11 +0530

    Time: 5 ms (42.67%), Space: 8.8 MB (46.91%) - LeetHub

---
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countSubstrings(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<vector<int>> dp1(n+1,vector<int>(m+1,0));
+        int ans=0;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    dp1[i][j]=dp1[i-1][j-1];
+                }
+                else{
+                    dp1[i][j]=1+dp[i-1][j-1];
+                }
+                ans+=dp1[i][j];
+            }
+        }
+        return ans;
+    }
+
+};

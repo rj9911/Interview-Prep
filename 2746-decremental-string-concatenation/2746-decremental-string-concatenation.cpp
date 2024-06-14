0553f82972ef6a6c114405658ebdecb3d98a3a43
ninjacoder <priyamgupta880@gmail.com>
2024-06-14 08:34:50 +0530

    Time: 120 ms (84.03%), Space: 44.9 MB (93.06%) - LeetHub

--- 2746-decremental-string-concatenation/2746-decremental-string-concatenation.cpp
+++ 2746-decremental-string-concatenation/2746-decremental-string-concatenation.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
     int minimizeConcatenatedLength(vector<string>& words) {
-        int dp[1000][26][26];
-        int n = words.size();
+        int res=INT_MAX,n=words.size()-1;
+        int dp[1001][26][26]={}; 
+        /*
+         DP states: dp[i][j][k]
+            i  -> current word
+            j  -> first letter of the string formed after considering the current word
+            k  -> last letter of the string formed after considering the current word
+        */
+        dp[0][words[0][0]-'a'][words[0].back()-'a']=words[0].length();
+        
+        for(int i=1;i<=n;i++)
+        {
+            int b = words[i][0]-'a'; //first letter of current word
+            int e = words[i].back()-'a'; //last letter of current word
+           
+            for(int j=0;j<26;j++)
+            {
+                for(int k=0;k<26;k++)
+                {
+                    if(dp[i-1][j][k]>0)
+                    { 
+                        int l;
+                        l=dp[i-1][j][k]+words[i].length();
 
-        // clear
-        for (int i = 0; i <n; i++) {
-            for (int j = 0; j <26; j++) {
-                for (int k = 0; k < 26; k++) {
-                    dp[i][j][k] = 50000;
+                        // Concate curr word + prev word
+                        if(dp[i][b][k]==0) dp[i][b][k] = (e==j)?l-1:l;
+                        else dp[i][b][k] = min((e==j)?l-1:l,dp[i][b][k]);
+
+                        // Concate prev word + curr word
+                        if(dp[i][j][e]==0) dp[i][j][e] = (b==k)?l-1:l;
+                        else dp[i][j][e] = min((b==k)?l-1:l,dp[i][j][e]);
+                    }
                 }
             }
         }
-
-        dp[0][words[0][0] - 'a'][words[0][words[0].size() - 1] - 'a'] = words[0].size();
-
-        for (int i = 1; i < n; i++) {
-            int l = words[i].size();
-            int firstChar = words[i][0] - 'a';
-            int lastChar = words[i][l - 1] - 'a';
-
-            for (int j = 0; j < 26; j++) {
-                for (int k = 0; k < 26; k++) {
-                    dp[i][firstChar][j] = min(dp[i][firstChar][j], dp[i - 1][k][j] + l - (k == lastChar ? 1 : 0));
-                    dp[i][j][lastChar] = min(dp[i][j][lastChar], dp[i - 1][j][k] + l - (k == firstChar ? 1 : 0));
-                }
+        for(int j=0;j<26;j++)
+        {
+            for(int k=0;k<26;k++)
+            {
+                if(dp[n][j][k]>0) res=min(res,dp[n][j][k]);
             }
         }
-
-        int ans = 50000;
-        int firstChar = words[n - 1][0] - 'a';
-        int lastChar = words[n - 1][words[n - 1].size() - 1] - 'a';
-        for (int i = 0; i < 26; i++) {
-            ans = min(ans, dp[n - 1][firstChar][i]);
-            ans = min(ans, dp[n - 1][i][lastChar]);
-        }
-
-        return ans;
-
+        return res;
     }
 
 };

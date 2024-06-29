83bcba19cfc1f8b826e63a91c0af60bd2dd6cc79
ninjacoder <priyamgupta880@gmail.com>
2024-06-29 20:59:12 +0530

    Time: 8 ms (10.87%), Space: 15.7 MB (5.62%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.cpp
+++ 0969-pancake-sorting/0969-pancake-sorting.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<int> pancakeSort(vector<int>& nums) {
-        int n = nums.size();
-        int j =n-1;
-        vector<int> res;
-        while(j>=0){
-            priority_queue<pair<int,int>> pq;
-            for(int i=j;i>=0;i--){
-                pq.push({nums[i],i});
-            }
-            auto it = pq.top();
-            if(it.second !=j ){
-                reverse(nums.begin(),nums.begin()+(it.second+1));
-                res.push_back(it.second+1);
-                reverse(nums.begin(),nums.begin()+j+1);
-                res.push_back(j+1);
-            }
-            j--;
+    std::vector<int> nums_copy = nums; // Make a copy of nums to sort
+    int n = nums_copy.size();
+    int j = n - 1;
+    std::vector<int> res;
+    while (j >= 0) {
+        std::priority_queue<std::pair<int, int>> pq;
+        for (int i = j; i >= 0; i--) {
+            pq.push({nums_copy[i], i});
         }
-        return res;
+        std::pair<int, int> it = pq.top();
+        if (it.second != j) {
+            std::reverse(nums_copy.begin(), nums_copy.begin() + (it.second + 1));
+            res.push_back(it.second + 1);
+            std::reverse(nums_copy.begin(), nums_copy.begin() + j + 1);
+            res.push_back(j + 1);
+        }
+        j--;
+    }
+    return res;
     }
 
 };

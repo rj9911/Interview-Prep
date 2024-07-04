f9d95192c01e6bf66a82532d6e45cff74f548ebb
ninjacoder <priyamgupta880@gmail.com>
2024-07-04 19:03:40 +0530

    Time: 33 ms (78.41%), Space: 40.9 MB (49.61%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int numFriendRequests(vector<int>& ages) {
-        unordered_map<int, int> num_reqs;
-        sort(ages.begin(), ages.end());
-        int cnt = 0;
-        int n = ages.size();
-        for (int i = n - 1; i >= 0; i--) {
-            int age_x = ages[i];
-            if (num_reqs.find(age_x) != num_reqs.end()) {
-                cnt += num_reqs[age_x];
-            } else {
-                double factored_age = 0.5 * age_x + 7;
-                int cnt_x = 0;
-                auto it  = upper_bound(ages.begin(), ages.begin() + i, factored_age);
-                if (it == ages.begin() + i) {
-                    num_reqs[age_x] = 0;
-                } else {
-                    int it_idx = distance(ages.begin(), it);
-                    cnt_x = (i - it_idx);
-                    num_reqs[age_x] = cnt_x;
-                    cnt += num_reqs[age_x];
+        sort(ages.begin(),ages.end());
+        int r = ages.size()-1;
+        int l = ages.size()-2;
+        int tot = 0;
+        int streak = 1;
+        while(r > 0){
+            while((l>=r) || l >= 0 && ages[l] > 0.5 * ages[r]+7){
+                if(ages[l] == ages[l+1] && ages[l] > 14){
+                    streak++;
                 }
-
+                else{
+                    tot+= streak*(streak-1)/2;
+                    streak = 1;
+                }
+                l--;
             }
+            l++;
+            tot+= r-l;
+            r--;
+            l--;
         }
-        return cnt;
+        tot+= streak*(streak-1)/2;
+        return tot;
     }
+
 
 };

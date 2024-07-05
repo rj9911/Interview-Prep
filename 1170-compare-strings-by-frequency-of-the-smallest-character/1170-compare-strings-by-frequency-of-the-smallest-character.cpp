bfa49fea6dbe987b924863ecd5b643c64b8457a8
ninjacoder <priyamgupta880@gmail.com>
2024-07-05 12:58:51 +0530

    Time: 39 ms (17.29%), Space: 19.7 MB (15.50%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-    vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) 
-    {
-        int q=queries.size(), w=words.size();
-        vector<int> ans(q,0);
-        vector<int> fq(q), fw(w);
-        for(int x=0; x<q; x++)
-        {
-            map<char,int> m;
-            for(int y=0; y<queries[x].size(); y++)
-            {
-                m[queries[x][y]]++;
-            }
-            fq[x]=m.begin()->second;
+    vector<int> numSmallerByFrequency(vector<string>& q, vector<string>& w) {
+        map<char,int>mp;
+        vector<int>p,r,ans;
+        for(int i=0;i<q.size();i++){
+            mp.clear();
+            for(int j=0;j<q[i].length();j++)mp[q[i][j]]++;
+            int k=0;
+            for(auto i:mp){k=i.second;break;}
+            p.push_back(k);
         }
-        for(int x=0; x<w; x++)
-        {
-            map<char,int> m;
-            for(int y=0; y<words[x].size(); y++)
-            {
-                m[words[x][y]]++;
-            }
-            fw[x]=m.begin()->second;
+        for(int i=0;i<w.size();i++){
+            mp.clear();
+            for(int j=0;j<w[i].length();j++)mp[w[i][j]]++;
+            int k=0;
+            for(auto i:mp){k=i.second;break;}
+            r.push_back(k);
         }
-        for(int x=0; x<q; x++)
-        {
-            for(int y=0; y<w; y++)
-            {
-                if(fq[x]<fw[y]) ans[x]++;
-            }
+        for(int i=0;i<p.size();i++){
+            int x=0;
+            for(int j=0;j<r.size();j++)if(p[i]<r[j])x++;
+            ans.push_back(x);
         }
         return ans;
     }
+
 
 };

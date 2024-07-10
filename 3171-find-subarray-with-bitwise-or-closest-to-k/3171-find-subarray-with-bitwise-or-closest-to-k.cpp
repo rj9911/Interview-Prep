f92684e1e620bb5813abba8d450fa0ee9829c82c
ninjacoder <priyamgupta880@gmail.com>
2024-07-10 08:30:22 +0530

    Time: 100 ms (99.85%), Space: 94.6 MB (98.80%) - LeetHub

--- 3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.cpp
+++ 3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
     int minimumDifference(vector<int>& nums, int k) {
-        int ans=INT_MAX,len=nums.size(),currNum,bitIndex,currXor=0,sum=0,start=0;
-        vector<vector<int>> preXor(len,vector<int>(32,0));
-        
-        for(int index=0;index<nums.size();index++) {
-            bitIndex=31;
-            currNum=nums[index];
-        
-            while(currNum>0) {
-                preXor[index][bitIndex]=currNum%2;
-                currNum/=2;
-                bitIndex--;
+        int n = nums.size();
+        int ans = INT_MAX;
+
+        ans = min(ans, abs(k - nums[0]));
+        if(n == 1) return ans;
+
+        int l = 0, r = 1;
+        int curr_l_val = nums[l];
+
+        while(r < n) {
+            int curr_val = curr_l_val | nums[r];
+
+            if(abs(k - curr_val) <= ans) ans = abs(k - curr_val);
+            if(ans == 0) return 0;
+
+            if(curr_val < k) {
+                r++;
+                curr_l_val = curr_val;
             }
-        }
-        
-        for(bitIndex=31;bitIndex>=0;bitIndex--) {
-            sum=0;
-            for(int index=0;index<nums.size();index++) {
-                sum+=preXor[index][bitIndex];
-                preXor[index][bitIndex]=sum;
-            }
-        }   
-        
-        for(int index=0;index<nums.size();index++) {
-            currXor=currXor|nums[index];
-            ans=min(ans,abs(currXor-k));
-           // cout<<start<<" "<<currXor<<endl;
-            while(start<index && currXor>k) {
-                currNum=currXor;
-                for(bitIndex=31;bitIndex>=0;bitIndex--) {
-                    if(currNum%2==1 && preXor[index][bitIndex]-preXor[start][bitIndex]==0)
-                        currXor-=pow(2,31-bitIndex);
-                    currNum/=2;
+            else {
+                l++;
+                curr_l_val = 0;
+                if(l == r) r++;
+
+                for(int i = l; i < r; i++) {
+                    curr_l_val |= nums[i];
                 }
-                //cout<<start<<"I "<<currXor<<endl;
-                start++;
-                ans=min(ans,abs(currXor-k));
             }
+
+            ans = min(ans, abs(k - curr_l_val));
         }
-        
+
         return ans;
-        
     }
+
 
 
 

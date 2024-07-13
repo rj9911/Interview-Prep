2363a25f5a109210eb9d5d301562908e824874ae
ninjacoder <priyamgupta880@gmail.com>
2024-07-13 15:51:47 +0530

    Time: 99 ms (68.68%), Space: 49.9 MB (70.48%) - LeetHub

---
+++ 1224-maximum-equal-frequency/1224-maximum-equal-frequency.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    #define ll long long
+    int maxEqualFreq(vector<int>& nums) {
+        ll n=nums.size(),answer=INT_MIN,count=INT_MIN;
+        unordered_map<ll,ll>mp,freq;
+        for(ll i=0;i<n;i++){
+            mp[nums[i]]++;
+            count=max(count,mp[nums[i]]);
+            freq[mp[nums[i]]]++;
+            if(mp[nums[i]]>1){
+                freq[mp[nums[i]]-1]--;
+            }
+            if(count==1){
+                answer=max(answer,i+1);
+            }
+            else{
+                if(mp.size()==1 && freq[i+1]==1){
+                    answer=max(answer,i+1);
+                }
+                else if(freq[count]==mp.size()-1 && (freq[count+1]==1 || freq[1]==1)){
+                    answer=max(answer,i+1);
+                }
+                else if(freq[count-1]==mp.size()-1 && freq[count]==1){
+                    answer=max(answer,i+1);
+                }
+            }
+        }
+        return answer;
+    }
+
+};

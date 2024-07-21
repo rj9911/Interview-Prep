2093b23fd596234b68d4d0dd8b20a7378ae624f7
ninjacoder <priyamgupta880@gmail.com>
2024-07-21 13:50:57 +0530

    Time: 23 ms (71.31%), Space: 14.9 MB (84.24%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
-    string largestPalindromic(string num) 
-    {
-        map<char,int>mp;
-        for(char ch:num) mp[ch]++;
-
-        int mx = INT_MIN;
-        for(auto [a, b]:mp)
-        {
-            if(b%2)
-            {
-                mp[a]--;
-                mx = max(mx, a-'0');
-            }
-        }
-
-        deque<char>opps;
-        if(mx != INT_MIN) opps.push_back(mx + '0');
-
-        for(auto [a, b]:mp)
-        {
-            while(b)
-            {
-                opps.push_back(a);
-                opps.push_front(a);
-                b -= 2;
-            }
-        }
-
-        while(opps.size() > 1 and opps.front()=='0') opps.pop_front();
-        while(opps.size() > 1 and opps.back()=='0') opps.pop_back();
-
+    string largestPalindromic(string s) {
+        vector<int> v(10);
+        for(char c:s)v[c-'0']++;
         string ans;
-        for(auto ch:opps) ans += ch;
+        int single=-1;
+        for(int i=9;i>=0;i--){
+            if(ans.empty() && i==0) continue;
+            while(v[i]>1){
+                ans.push_back(i+'0');
+                v[i]-=2;
+            }
+            if(v[i]==1 && single==-1)single=i;
+        }
+        string res=string(ans.rbegin(),ans.rend());
+        if(ans.empty() && single==-1) return "0";
+        if(single!=-1)
+            ans.push_back(single+'0');
+        ans+=res;
         return ans;
     }
+
 
 };

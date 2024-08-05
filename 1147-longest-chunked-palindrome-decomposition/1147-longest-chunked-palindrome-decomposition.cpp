5d298714f8a4a7b1fa92c95f357587ad85792f48
ninjacoder <priyamgupta880@gmail.com>
2024-08-05 07:53:56 +0530

    Time: 6 ms (43.65%), Space: 9.7 MB (56.35%) - LeetHub

---
+++ 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestDecomposition(string text) {
+        int ans=0;
+        int i=0;
+        int j=text.size()-1;
+        string s="",t="";
+        while(i<j){
+            s+=text[i];
+            t+=text[j];
+            
+            string rev=t;
+            reverse(rev.begin(),rev.end());
+
+            if(s==rev){
+                ans++;
+                t="";
+                s="";
+            }
+            i++;
+            j--;
+        }
+        ans*=2;
+        if(i==j || s.size()!=0) ans++;
+        return ans;
+    }
+
+
+};
